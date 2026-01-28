874fc8bd2c3d04f4dd9d07e6491c921840d97b58
Hikari Aufa Yafi <hikari32yafi@gmail.com>
2026-01-28 14:52:59 +0700

    commit all

---
+++ Soal1_RekursifString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class RekursifString
+{
+static void Main()
+    {
+        Console.WriteLine("tes soal 1");
+        Console.Write("Masukkan string: ");
+        string input = Console.ReadLine();
+
+
+        string hasil = ReverseString(input);
+        Console.WriteLine("Hasil: " + hasil);
+    }
+
+
+    static string ReverseString(string text)
+    {
+        if (text.Length == 0)
+            return "";
+
+        return ReverseString(text.Substring(1)) +"_"+ text[0];
+    }
+}
---
+++ Soal3_FindNumberGanda/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+class FindPrimeNumber
+{
+    static void Main()
+    {
+        Console.WriteLine("tes soal 3");
+        int[] numbers = { -3, 7, -3, 5, 9, 3, 5, 8, 5 };
+
+            List<int> duplicates = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int countDuplicate = 0;
+
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        
+                        
+                            countDuplicate++;
+                        
+                    }
+                }
+
+
+                if (countDuplicate > 1 && !duplicates.Contains(numbers[i]))
+                {
+                    if(numbers [i] ! = -3){
+                    duplicates.Add(numbers[i]);
+                    }
+                }
+            }
+                Console.WriteLine(string.Join(", ", duplicates));
+        }
+}
+
